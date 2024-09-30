f09bc2165493a53149867c35c58e544548223f19
lenafb <blackmon@berkeley.edu>
2024-09-30 15:58:38 -0700

    addition of argument parser

--- flowpy.py
+++ flowpy.py
@@ -1,5 +1,8 @@
-file2 = r'test'
 
-pre_df2 = pd.read_csv(file2)
 
-df1 = pre_df1[pre_df1['Gate']=='R6']
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required = True, help = "Path to csv file")
+
+pre_df2 = pd.read_csv(args["input"])
+
+df1 = pre_df1[pre_df1['Gate']==(args["gate"])]
